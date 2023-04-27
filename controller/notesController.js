a7a83939cc41788a94675f7e754f0a1c49856974
bear-muna <bearmuna@gmail.com>
2023-04-26 18:57:15 -0700

    added post note function into notesController

--- controller/notesController.js
+++ controller/notesController.js
@@ -12,3 +12,25 @@ router.get('/', (req, res) => {
         }
     })
 });
+
+router.post('/', (req, res) => {
+    fs.read('../db/db.json', 'utf-8', (err) => {
+        if (err) {
+            res.status(500).json({ msg: "Error reading db" });
+        } else {
+            const dataArr = JSON.parse(data);
+            const newNote = {
+                title: req.body.title,
+                test: req.body.text,
+            };
+            dataArr.push(newNote);
+            fs.writeFile('../db/db.json', JSON.stringify(dataArr, null, 4), (err) => {
+                if (err) {
+                    res.status(500).json({ msg: "Error writing db" });
+                } else {
+                    return res.json(newNote);
+                }
+            })
+        }
+    })
+})
